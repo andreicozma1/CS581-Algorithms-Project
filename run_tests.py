e30849d206d4349f5a65240fcee66e59da55b1c0
Andrei Cozma <me@andreicozma.com>
2023-04-19 17:21:27 -0400

    Updates

--- run_tests.py
+++ run_tests.py
@@ -5,8 +5,8 @@ import random
 num_tests = 10
 
 update_types = ["first_visit", "every_visit"]
-vals_eps = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-vals_gamma = [1.0, 0.99, 0.98, 0.97, 0.96, 0.95, 0.94, 0.93, 0.92, 0.91, 0.90]
+vals_eps = [0.1, 0.25, 0.5, 0.75, 0.9]
+vals_gamma = [1.0, 0.98, 0.95, 0.92, 0.9]
 
 
 def run_test(args):
