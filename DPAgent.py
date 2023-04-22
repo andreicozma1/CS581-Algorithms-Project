8ae24a2b1494b435b806469b634d26c65acef3fa
Landon Harris <lharri73@vols.utk.edu>
2023-04-22 17:45:01 -0400

    Updates

--- DPAgent.py
+++ DPAgent.py
@@ -4,20 +4,25 @@ from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 from matplotlib import pyplot as plt
 from tqdm import trange
 from Shared import Shared
+import warnings
 
 
 class DPAgent(Shared):
-    def __init__(self, theta=1e-10, **kwargs):
+    def __init__(self,/,**kwargs):
         super().__init__(**kwargs)
-        self.theta = theta
+        self.theta = kwargs.get('theta', 1e-10)
+        print(self.theta)
         self.V = np.zeros(self.env.observation_space.n)
-        self.Pi = None
+        self.Pi = np.zeros(self.env.observation_space.n, self.env.action_space.n)
+        if self.gamma >= 1.0:
+            warnings.warn("DP will never converge with a gamma value =1.0. Try 0.99?", UserWarning)
 
     def policy(self, state):
         return self.Pi[state]
 
-    def train(self, **kwargs):
+    def train(self, *args, **kwargs):
         i = 0
+        print(self.gamma)
         while True:
             delta = 0
             V_prev = np.copy(self.V)
@@ -38,10 +43,10 @@ class DPAgent(Shared):
             if delta < self.theta:
                 break
             i += 1
-            self.test()
+            # if i % 100 == 0 and i != 0:
+            #     self.test()
             print(f"Iteration {i}: delta={delta}")
             # break
-
         # policy = [self.policy(state, return_value=True)[0] for state in range(self.env.observation_space.n)]
         self.Pi = np.empty((self.env.observation_space.n, self.env.action_space.n))
         for s in range(self.env.observation_space.n):
@@ -51,23 +56,18 @@ class DPAgent(Shared):
                     # if state == self.env.observation_space.n-1: reward = 1
                     expected_value += probability * (reward + self.gamma * self.V[next_state])
                 self.Pi[s,a] = expected_value
-        self.Pi = np.argmax(self.Pi, axis=1)
-        print(self.Pi)
+        idxs = np.argmax(self.Pi, axis=1)
+        print(idxs)
+        self.Pi = np.zeros((self.env.observation_space.n,self.env.action_space.n))
+        self.Pi[np.arange(self.env.observation_space.n),idxs] = 1
+
+        # print(self.Pi)
         # return self.V, self.Pi
 
 
 if __name__ == "__main__":
     # env = gym.make('FrozenLake-v1', render_mode='human')
-    dp = DPAgent("FrozenLake-v1", is_slippery=False, desc=[
-        "SFFFFFFF",
-        "FFFFFFFH",
-        "FFFHFFFF",
-        "FFFFFHFF",
-        "FFFHFFFF",
-        "FHHFFFHF",
-        "FHFFHFHF",
-        "FFFHFFFG",
-    ])
+    dp = DPAgent(env_name="FrozenLake-v1")
     dp.train()
     dp.save_policy('dp_policy.npy')
     env = gym.make('FrozenLake-v1', render_mode='human', is_slippery=False, desc=[
@@ -84,8 +84,7 @@ if __name__ == "__main__":
     state, _ = env.reset()
     done = False
     while not done:
-        action = dp.policy(state)
-        action = np.argmax(action)
+        action = dp.choose_action(state)
         state, reward, done, _, _ = env.step(action)
         env.render()
 
