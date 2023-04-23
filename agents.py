6ee82fe7264b62c1e9478c00624812e7f88c35e2
Andrei Cozma <me@andreicozma.com>
2023-04-22 23:49:33 -0400

    Updates

--- agents.py
+++ agents.py
@@ -2,7 +2,13 @@
 from MCAgent import MCAgent
 from DPAgent import DPAgent
 
-AGENTS_MAP = {
-    "MCAgent": MCAgent,
-    "DPAgent": DPAgent
-}
+AGENTS_MAP = {"MCAgent": MCAgent, "DPAgent": DPAgent}
+
+
+def load_agent(agent_name, **kwargs):
+    if agent_name not in AGENTS_MAP:
+        raise ValueError(
+            f"ERROR: Agent '{agent_name}' not valid. Must be one of: {AGENTS_MAP.keys()}"
+        )
+
+    return AGENTS_MAP[agent_name](**kwargs)
